54cbfa87524119a66ee682e59ba6bd2d388b98ce
Alicia Villanueva <aliciav.texas@gmail.com>
2021-12-08 17:46:38 -0600

    "changed config path back to original to avoid errors. Imported form input fields"

--- src/components/Student_Facing/StudentRegistration.tsx
+++ src/components/Student_Facing/StudentRegistration.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth } from "../../../config/firebase";
+// Materia-UI
+import { makeStyles } from "@material-ui/core";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
 
 export default function StudentInterestForm(): JSX.Element {
   const [registering, setRegistering] = useState<boolean>(false);
@@ -10,6 +14,26 @@ export default function StudentInterestForm(): JSX.Element {
   const [error, setError] = useState<string>("");
 
   const navigate = useNavigate();
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      padding: theme.spacing(2),
+
+      "& .MuiTextField-root": {
+        margin: theme.spacing(1),
+        width: "300px",
+      },
+      "& .MuiButtonBase-root": {
+        margin: theme.spacing(2),
+      },
+    },
+  }));
+
+  const classes = useStyles();
 
   const signUpWithEmailAndPassword = () => {
     if (password !== confirm)
@@ -25,12 +49,42 @@ export default function StudentInterestForm(): JSX.Element {
       })
       .catch((error) => {
         console.log(error);
+        if (error.code.includes("auth/weak-password")) {
+          setError("Please enter a stronger password");
+        } else if (error.code.includes("auth/email-already-in-use")) {
+          setError("Email already in use");
+        } else {
+          setError("Unable to register. Please wait and try again");
+        }
+        setRegistering(false);
       });
   };
 
   return (
-    <div>
-      <h6>Student Interest Form</h6>
-    </div>
+    <form className={classes.root}>
+      Hey, there! Register to join our band family!
+      <TextField label="First Name" variant="filled" required />
+      <TextField label="Last Name" variant="filled" required />
+      <TextField
+        label="Email"
+        variant="filled"
+        type="email"
+        required
+        value={email}
+      />
+      <TextField
+        label="Password"
+        variant="filled"
+        type="password"
+        required
+        value={password}
+      />
+      <div>
+        <Button variant="contained">Cancel</Button>
+        <Button type="submit" variant="contained" color="primary">
+          Signup
+        </Button>
+      </div>
+    </form>
   );
 }
